b6007624b22b6a919530d94181edf7201dcdad32
Pratibha Singh <pratibha.ksingh98@gamil.com>
2026-06-17 23:22:37 +0530

    Upload full Spring Boot project

---
+++ src/test/java/com/project/ClientCarePro/ClientCareProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ClientCarePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCareProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
